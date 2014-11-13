15889ce01e46f4e605deb5fb3bb537340d3ed588
Tom Harrington <tph@atomicbird.com>
2014-11-13 15:00:17 -0700

    Add today extension target in Xcode

---
+++ DemoToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  DemoToday
+//
+//  Created by Tom Harrington on 11/13/14.
+//  Copyright (c) 2014 Atomic Bird LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
