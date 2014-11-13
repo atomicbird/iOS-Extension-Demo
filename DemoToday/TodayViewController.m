2cbde1da38dedcc9a620edbc25a086743d333e0f
Tom Harrington <tph@atomicbird.com>
2014-11-13 16:12:39 -0700

    Make the today extension use the shared framework and display notes.

--- DemoToday/TodayViewController.m
+++ DemoToday/TodayViewController.m
@@ -8,8 +8,13 @@
 
 #import "TodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
+#import <DemoSharedCode/DemoSharedCode.h>
+
+NSString *const kDemoNoteFilename = @"notes.bin";
 
 @interface TodayViewController () <NCWidgetProviding>
+
+@property (readwrite, strong) NSMutableArray *objects;
 
 @end
 
@@ -17,7 +22,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+
+    NSURL *groupURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.com.atomicbird.demonotes"];
+    NSURL *fileURL = [groupURL URLByAppendingPathComponent:kDemoNoteFilename];
+    
+    NSData *savedData = [NSData dataWithContentsOfURL:fileURL];
+    if (savedData != nil) {
+        NSArray *savedObjects = [NSKeyedUnarchiver unarchiveObjectWithData:savedData];
+        if (savedObjects != nil) {
+            self.objects = [savedObjects mutableCopy];
+        }
+    }
+    
+    if (self.objects == nil) {
+        self.objects = [NSMutableArray array];
+    }
+
+    self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, self.objects.count * 44.0 /* self.tableView.rowHeight*/);
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,6 +54,25 @@
     // If there's an update, use NCUpdateResultNewData
 
     completionHandler(NCUpdateResultNewData);
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.objects.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"demoTodayCell" forIndexPath:indexPath];
+    
+    DemoNote *object = self.objects[indexPath.row];
+    cell.textLabel.text = [object text];
+    cell.textLabel.textColor = [UIColor whiteColor];
+    return cell;
 }
 
 @end
